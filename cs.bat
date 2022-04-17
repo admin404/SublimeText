b43d78969a7e45fe44d5a059d6f85c09899d1d13
admin404 <neterror404@gmail.com>
2022-04-17 22:58:30 +0300

    17.04.2022_22:58:30,32

--- cs.bat
+++ cs.bat
@@ -4,3 +4,11 @@ set session_file1=%CD%\Data\Local\Session.sublime_session
 set session_file2=%CD%\Data\Local\Auto Save Session.sublime_session
 rm -rf %session_file1%
 rm -rf %session_file2%
+
+set current_date=%date%
+set current_time=%time%
+set comment=%date%_%time%
+
+git add .
+git commit -m "%comment%"
+git push
