191ee97bb7f934ebc6b3233c4614737ab3680f5d
admin404 <neterror404@gmail.com>
2022-04-17 22:53:13 +0300

    update

---
+++ cs.bat
@@ -0,0 +1,6 @@
+@echo off
+
+set session_file1=%CD%\Data\Local\Session.sublime_session
+set session_file2=%CD%\Data\Local\Auto Save Session.sublime_session
+rm -rf %session_file1%
+rm -rf %session_file2%
